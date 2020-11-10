969c45c71d93ca842429c88fa0797ea57c799a50
ZacJW <11651573+ZacJW@users.noreply.github.com>
2020-11-10 16:11:34 +0000

    Added support for built-in stylesheets

--- markdown-html.py
+++ markdown-html.py
@@ -10,6 +10,7 @@ REPO_PATH = pathlib.Path(os.environ['GITHUB_WORKSPACE'])
 INPUT_LIST = json.loads(os.environ['INPUT_INPUT_FILES'])
 OUTPUT_LIST = json.loads(os.environ['INPUT_OUTPUT_FILES'])
 EXCLUDE_DUPLICATES : bool = json.loads(os.environ['INPUT_EXCLUDE_DUPLICATES'])
+BUILTIN_STYLESHEET : str = json.loads(os.environ['INPUT_BUILTIN_STYLESHEET'])
 
 if not isinstance(INPUT_LIST, list) or not all([isinstance(sublist, list) for sublist in INPUT_LIST]):
     raise ValueError("input_files must be a JSON list of lists")
@@ -19,6 +20,12 @@ if not isinstance(OUTPUT_LIST, list):
 
 if len(OUTPUT_LIST) != len(INPUT_LIST):
     raise ValueError(f"input_files (length: {len(INPUT_LIST)}) must be the same length as output_files (length: {len(OUTPUT_LIST)})")
+
+if BUILTIN_STYLESHEET != "":
+    with open(REPO_PATH.joinpath(BUILTIN_STYLESHEET), 'r') as stylesheet_file:
+        style = "<style>\n" + stylesheet_file.read() + "</style>\n"
+else:
+    style = ""
 
 for input_sublist, output_path_str in zip(INPUT_LIST, OUTPUT_LIST):
     md.reset()
@@ -33,7 +40,7 @@ for input_sublist, output_path_str in zip(INPUT_LIST, OUTPUT_LIST):
                     md_str += input_file.read() + "\n"
     print("Generating", output_path_str)
     output_path = REPO_PATH.joinpath(output_path_str)
-    html = md.convert(md_str)
+    html = md.convert(md_str) + "\n" + style
     with open(output_path, 'w') as output_file:
         output_file.write(html)
 print("Markdown-HTML complete")
